43baf5a472d8a0a8cdac675dc343ce297bb0ddb5
Daniel Munoz <daniel.munoz@modmed.com>
2025-08-23 10:53:23 -0400

    1st commit with the project itself still need's more work

---
+++ dap-java-maven.el
@@ -0,0 +1,79 @@
+;;; dap-java-maven.el --- Debugging Java Maven tests in Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Your Name
+
+;; Author: Daniel Muñoz <demunoz2@uc.cl>
+;; Maintainer: Daniel Muñoz <demunoz2@uc.cl>
+;; Created: August 23, 2025
+;; Version: 0.1.0
+;; Keywords: dap, java, maven, test, debug
+;; Package-Requires: ((emacs "27.1") (dap-mode "0.5") (lsp-java "1.0"))
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;;
+;; This package provides a convenient way to debug single Java test methods
+;; in a Maven project using dap-mode. It launches the test using `mvnDebug`
+;; and attaches the debugger, bypassing the language server's test runner.
+;; This is particularly useful for complex projects using TestNG where the
+;; default DAP test integration fails.
+
+;;; Code:
+
+(require 'lsp-java)
+(require 'dap-mode)
+(require 'dap-java)
+
+(defun dap-java-maven-run-test-at-point ()
+  "Run the single Maven test at point from the project's root directory."
+  (interactive)
+  ;; Use the built-in project.el to find the project.
+  (if-let (project (project-current))
+      ;; If a project is found, proceed.
+      (let* ((project-root (project-root project))
+             (test-to-run (dap-java-test-method-at-point)))
+        (if test-to-run
+            (dap-start-debugging (list :program-to-start (format "mvn -Dtest=%s test" test-to-run)
+                                       :name test-to-run
+                                       :cwd project-root
+                                       :skip-debug-session t))
+          (message "Could not find a test method at point.")))
+    ;; If no project is found, print an error.
+    (message "Not inside a known project.")))
+
+;; Run maven clean install skipping tests
+(defun dap-java-maven-build-project ()
+  "Run =\'mvn clean install -DskipTests=\' in the project's root directory."
+  (interactive)
+  ;; Use the built-in project.el to find the project.
+  (if-let (project (project-current))
+      ;; If a project is found, set the directory and run the command.
+      (let ((default-directory (project-root project)))
+        (compile "mvn clean install -DskipTests"))
+    ;; If no project is found, print an error.
+    (message "Not inside a known project.")))
+
+(defun dap-java-maven-debug-test-at-point ()
+  "Debug the test at point using Maven launched by dap-mode."
+  (interactive)
+  (let* ((port (dap--find-available-port))
+         (test-to-run (dap-java-test-method-at-point))
+         (project-root (project-root (project-current)))
+         (debug-command (format "mvn -Dmaven.surefire.debug=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=%s -Dtest=%s test"
+                                port test-to-run))
+         )
+    (dap-debug
+     (list :name (format "Debug %s" test-to-run)
+           :type "java"
+           :request "attach"
+           :hostName "localhost"
+           :wait-for-port t
+           :port port
+           :cwd project-root
+           :program-to-start debug-command))
+    ))
+
+(provide 'dap-java-maven)
+
+;;; dap-java-maven.el ends here
