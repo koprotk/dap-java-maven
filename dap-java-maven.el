cd7527877d34b4751d30d19c5ca6a85238b573e3
koprotk <109672714+koprotk@users.noreply.github.com>
2025-08-29 08:50:58 -0400

    Update dap-java-maven.el
    Added test-class method, my name on the commentary, and fixed unused variables.

--- dap-java-maven.el
+++ dap-java-maven.el
@@ -1,6 +1,6 @@
 ;;; dap-java-maven.el --- Debugging Java Maven tests in Emacs -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025 Your Name
+;; Copyright (C) 2025 Daniel Muñoz
 
 ;; Author: Daniel Muñoz <demunoz2@uc.cl>
 ;; Maintainer: Daniel Muñoz <demunoz2@uc.cl>
@@ -12,8 +12,9 @@
 ;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
-;; Test and debug package for maven projects. For debugging calls 
-;; mvn surefire plugin, so should be able on your maven project.
+;; Test and debug package for maven testing projects. For debugging  
+;; calls mvn surefire plugin, so it should be able to work on your 
+;; maven project.
 
 ;;; Code:
 
@@ -25,10 +26,26 @@
   "Run the single Maven test at point from the project's root directory."
   (interactive)
   ;; Use the built-in project.el to find the project.
-  (if-let (project (project-current))
+  (if (lsp-java--get-root)
       ;; If a project is found, proceed.
-      (let* ((project-root (project-root project))
-             (test-to-run (dap-java-test-method-at-point)))
+      (let* ((test-to-run (dap-java-test-method-at-point)))
+        (if test-to-run
+            (dap-start-debugging (list :program-to-start (format "mvn -Dtest=%s test" test-to-run)
+                                       :name test-to-run
+                                       :environment-variables `(("TERM" . "xterm-256color"))
+                                       :cwd (lsp-java--get-root)
+                                       :skip-debug-session t))
+          (message "Could not find a test method at point.")))
+    ;; If no project is found, print an error.
+    (message "Not inside a known project.")))
+
+(defun dap-java-maven-run-test-class ()
+  "Run the single Maven test at point from the project's root directory."
+  (interactive)
+  ;; Use the built-in project.el to find the project.
+  (if (lsp-java--get-root)
+      ;; If a project is found, proceed.
+      (let* ((test-to-run (dap-java-test-class)))
         (if test-to-run
             (dap-start-debugging (list :program-to-start (format "mvn -Dtest=%s test" test-to-run)
                                        :name test-to-run
@@ -41,13 +58,13 @@
 
 ;; Run maven clean install skipping tests
 (defun dap-java-maven-build-project ()
-  "Run =\'mvn clean install -DskipTests=\' in the project's root directory."
+  "Build and update dependencies maven project skipping tests."
   (interactive)
   ;; Use the built-in project.el to find the project.
   (if-let (project (project-current))
       ;; If a project is found, set the directory and run the command.
       (let ((default-directory (project-root project)))
-        (compile "mvn clean install -DskipTests"))
+        (compile "mvn clean install -DskipTests -U"))
     ;; If no project is found, print an error.
     (message "Not inside a known project.")))
 
